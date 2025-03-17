7da76a444f983ad06683b98c6f16b44446259eca
alyssamaeburgos <burgos.alyssamae2@gmail.com>
2025-03-17 22:29:21 +0800

    create tasks table and create relationship of  user and task in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function tasks() 
+    {
+        return $this->hasMany(Task::class);
+    }
 }
